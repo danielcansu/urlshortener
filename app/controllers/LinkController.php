b14ded7d05f36b3a5b3707a0287077bcf52fd211
danielcansu <danielcansu@gmail.com>
2015-05-20 12:55:21 +0200

    url short with larval

---
+++ app/controllers/LinkController.php
@@ -0,0 +1,59 @@
+<?php
+
+class LinkController extends BaseController {
+	public function make(){
+
+		$validator = Validator::make(Input::all(), array(
+
+			'url' => 'required|url|max:255'
+			));
+
+		if($validator->fails()) {
+			return Redirect::action('home')->withInput()->withErrors($validator);
+		}else{
+			
+			$url = Input::get('url');
+			$code = null;
+
+			$exists = Link::where('url', '=', $url);
+
+			if($exists->count() === 1){
+				$code = $exists->first()->code;
+
+			} else {
+				
+				$created = Link::create(array(
+					'url' => $url
+
+					));
+
+				if($created){
+					$code = base_convert($created->id, 10, 36);
+
+					Link::where('id', '=', $created->id)->update(array(
+
+						'code' => $code
+
+						));
+				}
+			}
+
+			if($code){
+			 return Redirect::action('home')->with('global', 'All done! Here is your short URL: <a href="' . URL::action('get', $code) . '">' . URL::action('get', $code) . '</a>');
+			}
+		}
+
+		return Redirect::action('home')->with('global', 'Something went wrong, try again');
+	}
+
+	public function get($code){
+
+		$link = Link::where('code', '=', $code);
+
+		if($link->count() === 1){
+			return Redirect::to($link->first()->url);
+		}
+
+		return Redirect::action('home');
+	}
+}
---
+++ app/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>URL Shortener</title>
+		<link rel="stylesheet" href="{{ URL::to('css/global.css') }}">
+	</head>
+	<body>
+		<div class="container">
+			<h1 class="title">Shorten a URL</h1>
+
+				@if($errors->has('url'))
+
+					<p> {{ $errors->first('url') }}</p>
+
+				@endif
+
+				@if(Session::has('global'))
+					<p>{{ Session::get('global') }}</p>
+
+				@endif
+
+
+			<form action=" {{ URL::action('make') }}" method="post">
+				<input type="url" name="url" placeholder="Enter a URL" autocomplete="off"{{ Input::old('url') ? ' value="' . e(Input::old('url')) . '"' : '' }}>
+				<input type="submit" value="Shorten">
+			</form>
+		</div>
+	</body>
+</html>
+
